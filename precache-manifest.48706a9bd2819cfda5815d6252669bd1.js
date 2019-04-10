4fa98d252c021fdc4cf4368f32a7c16460fa7dda
Rajat Jain <mznrajatjain@gmail.com>
2019-04-10 15:44:46 +0530

    Updates

---
+++ precache-manifest.48706a9bd2819cfda5815d6252669bd1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4ca9822c1c98f8c3874b",
+    "url": "/phone-directory-reactjs/static/js/runtime~main.d626addd.js"
+  },
+  {
+    "revision": "7fe025aebe37b35fc4ac",
+    "url": "/phone-directory-reactjs/static/js/main.e270c9ee.chunk.js"
+  },
+  {
+    "revision": "682bb10688a65cadd4dc",
+    "url": "/phone-directory-reactjs/static/js/2.4bcb3ee1.chunk.js"
+  },
+  {
+    "revision": "7fe025aebe37b35fc4ac",
+    "url": "/phone-directory-reactjs/static/css/main.522b9c68.chunk.css"
+  },
+  {
+    "revision": "196c75334f9f2b99aeb11fc616e89794",
+    "url": "/phone-directory-reactjs/index.html"
+  }
+];
